679b6fc821c8457231cd88aaae43784667ac6e6d
Eunice Oboravo <nicenonye7@gmail.com>
2022-12-17 02:31:39 -0800

    i

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
 *main - Entry point
-*Return: Always 0 (Success)
+*Return: Always 0 (Clever)
 */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("\"with proper grammer, but the outcome is a piece of art,");
 	return (0);
 }
