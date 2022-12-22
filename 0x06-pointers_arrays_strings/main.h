56332e91308fdcf656d61867ff999f018849210c
Eunice Oboravo <nicenonye7@gmail.com>
2022-12-22 12:00:15 -0800

    programming

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
  *int prototypes()
  *char prototypes()
  */
-void reverse_arrays(int *a, int n);
+void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
